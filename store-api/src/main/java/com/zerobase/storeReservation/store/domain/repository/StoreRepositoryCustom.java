dc188dc767da5f25d3bc684a8a231dfb79e0374d
ming <123580123+mingkiii@users.noreply.github.com>
2023-06-17 23:24:29 +0900

    Delete store-api directory

--- store-api/src/main/java/com/zerobase/storeReservation/store/domain/repository/StoreRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.zerobase.storeReservation.store.domain.repository;
-
-import com.zerobase.storeReservation.store.domain.dto.StoreDto;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Sort;
-
-public interface StoreRepositoryCustom {
-    Page<StoreDto> getNearbyStores(double latitude, double longitude, double maxDistance, int page, int pageSize, Sort sort);
-}
