0b79fddeff9d442f9e5dc648f39e632439462b39
mingkiii <kimchunjae92@gmail.com>
2023-06-23 19:10:34 +0900

    feat : [015-MODEL_VALIDATION] 유효성 검사 에러 처리

--- member-api/src/main/java/com/zerobase/storeReservation/member/exception/ErrorCode.java
+++ member-api/src/main/java/com/zerobase/storeReservation/member/exception/ErrorCode.java
@@ -13,7 +13,8 @@ public enum ErrorCode {
     // signIn
     LOGIN_CHECK_FAIL(HttpStatus.BAD_REQUEST, "이메일과 비밀번호를 확인 해주세요."),
 
-    NOT_FOUND_MEMBER(HttpStatus.BAD_REQUEST, "회원을 찾을 수 없습니다.");
+    NOT_FOUND_MEMBER(HttpStatus.BAD_REQUEST, "회원을 찾을 수 없습니다."),
+    VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "올바른 입력값이 아닙니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
