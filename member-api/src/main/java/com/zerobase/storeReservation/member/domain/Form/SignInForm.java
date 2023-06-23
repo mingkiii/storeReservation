3abc6936064084fe66d834aef78d02b7fba38a8c
ming <123580123+mingkiii@users.noreply.github.com>
2023-06-23 18:54:39 +0900

    Update SignInForm.java

--- member-api/src/main/java/com/zerobase/storeReservation/member/domain/Form/SignInForm.java
+++ member-api/src/main/java/com/zerobase/storeReservation/member/domain/Form/SignInForm.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 @Builder
 public class SignInForm {
     @NotBlank(message = "필수 입력")
-    @Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$", message = "이메일 형식이 올바르지 않습니다.")
+    @Email(message = "이메일 형식에 맞게 입력해 주세요.")
     private String email;
     @NotBlank(message = "필수 입력")
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요. 특수문자는 반드시 포함 해주세요.")
